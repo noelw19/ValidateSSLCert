bc677dfb48198205b464f2caf0fdf8df591c6900
noelw19 <noelw19@outlook.com>
2024-03-05 20:01:23 +1300

    Added cleaner error handling

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 )
 
 func main() {
-	conn,err := tls.Dial("tcp", "example.com:80", nil)
-	if err != nil {
-		panic("Server doesnt support SSL cert. Err: " + err.Error())
-	}
+	conn,err := tls.Dial("tcp", "google.com:443", nil)
+	must(err, "erver doesn't support SSL certificate err: ")
+	// check SSL and site hostname match
+	er := conn.VerifyHostname("go4ogle.com")
+	must(er, "Hostname doesn't match with certificate: ")
+	fmt.Println(conn)
+}
 
+func must(err error, s string) {
+	if err != nil {
+		panic(s + err.Error())
+	}
 }
